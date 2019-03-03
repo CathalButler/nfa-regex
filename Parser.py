7421dc2e1f2479c47c88a276b7db57618eb85610
Cathal <cathalbutler@outlook.com>
2019-03-03 21:37:45 +0000

    Added a runner class. More work done on the infix to postfix parser.

--- Parser.py
+++ Parser.py
@@ -1,61 +1,54 @@
 # Cathal Butler - G00346889 - Graph Theory Project
 # Parser class
-
-# Variables
-operation = []
-output = []
-token = []
-i = 1
+# https://brilliant.org/wiki/shunting-yard-algorithm/
+# http://www.oxfordmathcenter.com/drupal7/node/628
 
 
 # Defined function
 # Function to read a string and convert infix to postfix
-def infixcoventer(user_input):
+def infix_to_postfix_conversion(user_input):
+    # Variables
+    specials = {'*': 50, '.': 40, '|': 30}
+    postfix = ''
+    stack = ''
+
     for char in user_input:
-        print(char)
-        # Pass chars into token list:
-        token = char
+        if char == '(':
+            stack = stack + char  # if '('  add to the stack
+        elif char == ')':
+            while stack[-1] != '(':  # while he char at the end of the string:
+                postfix = postfix + stack[-1]  # add to the postfix
+                stack = stack[:-1]  # removing from the top of the stack
+            stack = stack[:-1]  # remove the open '(' bracket from the stack
+        elif char in specials:  # if the chars are in the dictionary process:
+            # loop through the stack and check the precedence of each char, get the char provided, if none then 0) <= get char at the end of the stack[-1] if none then 0
+            while stack and specials.get(char, 0) <= specials.get(stack[-1], 0):
+                # remove from the top of the stack and add it to the postfix expression:
+                postfix, stack = postfix + stack[-1], stack[:-1]
+            stack = stack + char  # push the special char onto the stack!!!
 
-        # of token is white space skip it
-        if token is ' ':
-            continue
+        else:
+            postfix = postfix + char  # append postfix expression
 
-        # if its a number it to the queue
-        if token.isalpha() or token.isdigit():
-            output.append(token)
-
-
-
-# End infixconveter function
-
-# Function to return the order or precedence of operators via numbers 1-3:
-def precedence(token):
-    ##if token is '*' || token is '.' || token is '|':
-       ## while operation != 0 &&
-     ##       if not operation:
+    # if anything is left, push them onto the end of the postfix expression
+    while stack:  # loop through the stack
+        postfix = postfix + stack[-1]  # add to postfix
+        stack = stack[:-1]  # removing from the top of the stack
+    return postfix
 
 
-
-# end precedence function
-
-def order_of_precedence:
-    ##if token is '*'
+# End infix_to_postfix_conversion function
 
 # Main menu function
-def mainmenu():
+def main_menu():
     # Heading
     print("=======================================================================================")
     print("==========                        Cathal Butler                              ==========")
     print("==========   Parse the regular expression from infix to postfix notation     ==========")
     print("=======================================================================================")
 
-    user_input = input('\nPlease enter a some values ')
+    # user_input = input('\nPlease enter a some values ')
 
-    infixcoventer(user_input)
+    user_input = "(a.b)|(c*.d)"  # hardcoded for testing
 
-
-# End mainmenu function.
-
-# Called function
-# Run main menu
-mainmenu()
+    print("Complete conversion from infix to postfix:\n ", infix_to_postfix_conversion(user_input))  # print output of infix convert
---
+++ Runner.py
@@ -0,0 +1,6 @@
+# Cathal Butler - G00346889 - Graph Theory Project
+# Runner class
+import Parser
+
+# Run main_menu
+Parser.main_menu()
